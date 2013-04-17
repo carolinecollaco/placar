4ad9936545206fd4522324356e7e95e8c38308b7
carol <carol@data.(none)>
2013-04-17 18:17:36 -0300

    Usei o contador de segundos para contar o tempo de osaeki quando tenho um yuko

--- src/Score.java
+++ src/Score.java
@@ -1,14 +1,25 @@
-
+/**
+ * Na classe score temos os tipos de pontuações que podem existir em uma competição de Judô
+ * Temos também o tempo de osaekomi, que pode resultar em uma pontuação proporcioanal ao tempo adquirido.
+ * @author Caroline Collaço and
+ * @version 2013.17.03
+ */
 public class Score {
-
+	//pontuação wazari
 	private int wazari;
+	//pontuaçãp yuko
 	private int yuko;
+	//tempo de osaekomi quando é equivalente a um Ippon
 	private CountSeconds osaekomiIppon;
+	//tempo de osaekomi quando é equivalente a um Wazari
 	private CountSeconds osaekomiWazari;
-	
+	//tempo de osaekomi quando é equivalente a um Yuko
+	private CountSeconds osaekomiYuko;
+
 	public Score() {
 		osaekomiIppon = new CountSeconds();
 		osaekomiWazari = new CountSeconds();
+		osaekomiYuko = new CountSeconds();
 	}
 	
 	public void addWazariAndStartOsaekomi() {
@@ -29,6 +40,11 @@ public class Score {
 			wazari += 1;
 			yuko= 0;
 		}
+		osaekomiYuko.start();
+	}
+
+	public long getTimeOsaekomiYuko() {
+		return osaekomiYuko.getTimePassedInSeconds();
 	}
 	
 	public int getYukko() {		
